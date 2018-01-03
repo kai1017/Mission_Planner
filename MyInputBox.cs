c2bd793fdab94c4875e6084b5f37e1edf1d3f13f
kyle_weng <106753033@nccu.edu.tw>
2018-01-03 21:31:11 +0800

    2018/1/3
    初始版本

---
+++ MyInputBox.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MissionPlanner
+{
+    public class MyInputBox: Form
+    {
+        private Button button2;
+        private Button button1;
+
+        public MyInputBox()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // MyInputBox
+            // 
+            this.ClientSize = new System.Drawing.Size(307, 212);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MyInputBox";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
